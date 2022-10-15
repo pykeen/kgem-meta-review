334c77cd413b120c6c9a57e80db76b504cd9ec49
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 06:02:58 +0000

    Adding tarfile member sanitization to extractall()

--- autoreview/prereviewer.py
+++ autoreview/prereviewer.py
@@ -123,7 +123,26 @@ def check(arxiv_id: str):
     download_path = MOD.ensure(name=f'{arxiv_id}.tar.gz', url=f'https://arxiv.org/e-print/{arxiv_id}')
     extract_directory = MOD.join(arxiv_id)
     with tarfile.open(download_path) as tar:
-        tar.extractall(path=extract_directory)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=extract_directory)
 
     counter = Counter()
     ci_counter = Counter()
